1eca411d424c353c6a7641c0b79756aea9acd593
Julie Thrall <juliethrall10@gmail.com>
2019-09-05 09:44:53 -0700

    adding dotenv file

--- api/server.js
+++ api/server.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const helmet = require('helmet');
+const db = require('../data/db')
+db.connect({
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS
+})
 
 const apiRouter = require('./api-router.js');
+console.log('environment:', process.env.NODE_ENV);
 
 const server = express();
 
